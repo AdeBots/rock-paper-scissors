9c1bcdb5015dafbc358302e8b25efaeabb6e0101
AdeBots <190976863+AdeBots@users.noreply.github.com>
2024-12-15 01:13:34 +0100

    get Computer choice

---
+++ script.js
@@ -0,0 +1,16 @@
+function random(num1, num2){
+    return Math.ceil(Math.random() * (num2 - num1) + num1);
+}
+function getComputerChoice (){
+    let compChoice = random(0,3);
+    if(compChoice === 1){
+        console.log("rock");
+    }
+    else if(compChoice === 2){
+       console.log("paper");
+    }
+    else{
+        console.log("scissors");
+    }
+}
+
