4ffeed0243d8c8e2c3941e63f82a0da624bd6255
AdeBots <190976863+AdeBots@users.noreply.github.com>
2024-12-15 02:03:52 +0100

    Add logic to determine winner

--- script.js
+++ script.js
@@ -15,13 +15,52 @@ function getComputerChoice (){
     }
     return computerChoice;
 }
-getComputerChoice ();
 function getHumanChoice(){
     let humanChoice;
-    humanChoice = prompt("Rock, paper, scissors?");
-    return humanChoice
+    humanChoice = prompt("Rock, paper, scissors?").toLowerCase;
+    return humanChoice;
 }
-getHumanChoice ();
 let computerScore = 0;
 let humanScore = 0;
-
+function playRound(){
+    getComputerChoice;
+    getHumanChoice;
+   if(getComputerChoice == "rock"){
+    if(getHumanChoice == "scissors"){
+        console.log("You lose! " + getComputerChoice() + " beats " + getHumanChoice());
+    }
+    computerScore++;
+   }
+   else if(getComputerChoice == "scissors"){
+    if(getHumanChoice == "paper"){
+        console.log("You lose! " + getComputerChoice() + " beats " + getHumanChoice());
+    }
+    computerScore++;
+   }
+   else if(getComputerChoice = "paper"){
+    if(getHumanChoice = "rock"){
+        console.log("You lose! " + getComputerChoice() + " beats " + getHumanChoice());
+    }
+    computerScore++;
+   } else if(getComputerChoice == "rock"){
+    if(getHumanChoice == "paper"){
+        console.log("You win! " + getHumanChoice() + " beats " + getComputerChoice());
+    }
+    humanScore++;
+   }
+   else if(getComputerChoice == "scissors"){
+    if(getHumanChoice == "rock"){
+        console.log("You win! " + getHumanChoice() + " beats " + getComputerChoice());
+    }
+    humanScore++;
+   }
+   else if(getComputerChoice = "paper"){
+    if(getHumanChoice = "scissors"){
+        console.log("You win! " + getHumanChoice() + " beats " + getComputerChoice());
+    }
+    humanScore++;
+   }
+   else{
+    console.log("Tie");
+   }
+}
