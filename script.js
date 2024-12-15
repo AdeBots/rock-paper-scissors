1e669ab9a4c18e93678c1fdd861e9710780f8b30
AdeBots <190976863+AdeBots@users.noreply.github.com>
2024-12-15 02:26:02 +0100

    convert random numbers into strings

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ function getComputerChoice(){
         randomNumber = Math.ceil(Math.random() * (num2 - num1)+ num1);
         return randomNumber
     }
-    console.log(random(0,3));
+    compChoice = random(0, 3);
+    if(compChoice === 1){
+        return "rock";
+    }
+    else if(compChoice === 2){
+        return "paper";
+    }
+    else if(compChoice === 3){
+        return "scissors";
+    }
 }
-getComputerChoice();
+console.log(getComputerChoice());
